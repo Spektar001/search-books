78abb38c3be8d5bb02d03f735ce7901a50d5786c
Harry-Gupenec <Spektar001@mail.ru>
2023-09-14 15:21:38 +0300

    feat: added error output on book[id] page

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -9,42 +9,48 @@ const BookPage = ({ params }: { params: { id: string } }) => {
   const dispatch = useAppDispatch();
   dispatch(setId(params.id));
   let itemId = useAppSelector((state) => state.bookByIdSlice.id);
-  const { data } = useGetBookByIdQuery(itemId);
+  const { data, isError } = useGetBookByIdQuery(itemId);
 
   return (
     <div className="book">
-      {data?.volumeInfo.imageLinks ? (
-        <Image
-          src={data?.volumeInfo.imageLinks?.thumbnail}
-          width={450}
-          height={550}
-          alt="book_img"
-          className="book__image"
-        />
+      {isError ? (
+        <h2 style={{ textAlign: "center", fontSize: "32px" }}>ERROR!</h2>
       ) : (
-        <Image
-          src={"/no_image.png"}
-          width={100}
-          height={100}
-          alt="book_img"
-          className="book__image"
-        />
+        <>
+          {data?.volumeInfo.imageLinks ? (
+            <Image
+              src={data?.volumeInfo.imageLinks?.thumbnail}
+              width={450}
+              height={550}
+              alt="book_img"
+              className="book__image"
+            />
+          ) : (
+            <Image
+              src={"/no_image.png"}
+              width={100}
+              height={100}
+              alt="book_img"
+              className="book__image"
+            />
+          )}
+          <h2 className="book__title">{data?.volumeInfo.title}</h2>
+          <div className="book__info">
+            <p className="book__info_category">
+              <span className="info">Category: </span>
+              {data?.volumeInfo.categories?.join(", ")}
+            </p>
+            <p className="book__info_authors">
+              <span className="info">Authors: </span>
+              {data?.volumeInfo.authors?.join(", ")}
+            </p>
+            <p className="book__info_description">
+              <span className="info">Description: </span>
+              {data?.volumeInfo.description}
+            </p>
+          </div>
+        </>
       )}
-      <h2 className="book__title">{data?.volumeInfo.title}</h2>
-      <div className="book__info">
-        <p className="book__info_category">
-          <span className="info">Category: </span>
-          {data?.volumeInfo.categories?.join(", ")}
-        </p>
-        <p className="book__info_authors">
-          <span className="info">Authors: </span>
-          {data?.volumeInfo.authors?.join(", ")}
-        </p>
-        <p className="book__info_description">
-          <span className="info">Description: </span>
-          {data?.volumeInfo.description}
-        </p>
-      </div>
     </div>
   );
 };
